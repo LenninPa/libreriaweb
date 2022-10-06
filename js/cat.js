16ec24f56ff4b57f2391a15e05a74b1125d6ce5d
Lennin Palacios <leninpalacios055@gmail.com>
2022-10-06 02:54:53 -0600

    Version 1

---
+++ js/cat.js
@@ -0,0 +1,25 @@
+let container = document.getElementById('CS_Container');
+let close = document.querySelector(".container_Categories_BTTClose")
+let main = document.getElementById("MCSection")
+function ShowCat(){
+	container.style.transition = '.5s';
+	container.style.visibility = 'visible';
+	container.style.opacity = '1';
+	container.style.overflow = 'auto';
+	container.style.zIndex = '100';
+	main.style.opacity = '0';
+
+	
+}
+
+function CloseCat(){
+	container.style.transition = '.5s';
+	container.style.opacity = '0';
+	container.style.visibility = 'hidden';
+	container.style.overflow = 'hidden';
+	container.style.zIndex = '-100';
+	main.style.opacity = '100';
+
+}
+
+close.addEventListener("click", CloseCat)
