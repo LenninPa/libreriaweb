bfd5f8a50bce140338e855ae73b4e163633fd1d5
Lennin <leninpalacios055@gmail.com>
2022-10-09 08:59:22 -0600

    Solo agrege el index.php

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Catalogo Libreria</title>
+	<meta name="Catalogo" description="Papaleria, oficina y mas">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, minimun-scale=0, maximun-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Advent+Pro:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+	<main class="container" id="MCSection">
+		<div class="container_Info" id="CI_Section">
+			<span class="container_Info_Box">
+				<h1 class="container_info_box_tititle">Catalogo Libreria</h1>
+				<h5 class="container_info_box_Description">Haz tus <br>Consultas a nuestro <br><a href="" class="container_info_box_Description_link">WhattsApp</a></h5>
+			</span>
+		</div>	
+
+
+		<img src="img/optimized/notebook.png" alt="Catalogo Libreria" class="container_img_center">
+
+
+
+
+		<div class="container_ShowCategories">
+				<a href="" class="container_ShowCategories_Bttn">Ver productos</a>
+				<span class="container_ShowCategories_Bttn-M">O elige por</span>
+				<span class="container_ShowCategories_Bttn" onclick="ShowCat()">Categorias</span>
+		</div>
+	</main>
+
+	<section class="container_Categories" id="CS_Container">
+		<div class="container_Categories_BTTClose">Cerrar</div>
+
+
+		<div class="container_Categories_display">
+
+			<span class="container_Categories_display_content">
+				<img src="img/optimized/papeleria.png" alt="Categorias" class="container_Categories_display_Content_IO">
+				<a href="" class="container_Categories_display_Content_IO_Titles">Papeleria</a>
+			</span>
+
+			<span class="container_Categories_display_content">
+				<img src="img/optimized/escolar.png" alt="Categorias" class="container_Categories_display_Content_IO">
+				<a href="" class="container_Categories_display_Content_IO_Titles">Escolar</a>
+			</span>
+
+			<span class="container_Categories_display_content">
+				<img src="img/optimized/adhesivos.png" alt="Categorias" class="container_Categories_display_Content_IO">
+				<a href="" class="container_Categories_display_Content_IO_Titles">Adhesivos</a>
+			</span>
+
+			<span class="container_Categories_display_content">
+				<img src="img/optimized/oficina.png" alt="Categorias" class="container_Categories_display_Content_IO">
+				<a href="" class="container_Categories_display_Content_IO_Titles">Oficina</a>
+			</span>
+
+			<span class="container_Categories_display_content">
+				<img src="img/optimized/decoracion.png" alt="Categorias" class="container_Categories_display_Content_IO">
+				<a href="" class="container_Categories_display_Content_IO_Titles">Fiesta</a>
+			</span>
+
+			<span class="container_Categories_display_content">
+				<img src="img/optimized/servicios.png" alt="Categorias" class="container_Categories_display_Content_IO">
+				<a href="" class="container_Categories_display_Content_IO_Titles">Servicios</a>
+			</span>
+
+
+		</div>
+	</section>
+
+
+	
+	<script src="js/main.js"></script>
+</body>
+</html>
